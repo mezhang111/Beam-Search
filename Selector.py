bdc9b670f4306f5d3f9a9401b873d61873b24998
Mengtao Zhang <mengtao.zhang@inf.ethz.ch>
2021-10-03 15:08:39 +0200

    Class Selector and subclasses. Not finished.

---
+++ Selector.py
@@ -0,0 +1,21 @@
+
+class Selector(object):
+	def select(self, arr, k): #given an arry and k, give back the top k biggest values back
+		pass
+
+class BubbleSelector(Selector):
+	def select(self, arr, k):
+		pass
+
+class HeapSelector(Selector):
+	def select(self, arr, k):
+		pass
+
+class DeterministicSelector(Selector):
+	def select(self, arr, k):
+		pass
+
+class RandomSelector(Selector):
+	def select(self, arr, k):
+		pass
+
